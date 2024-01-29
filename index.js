c47ea777e8e3e5c73b1ccae4b8d7063bfc2e2392
akhil <akhiljawali91@gmail.com>
2024-01-29 17:20:54 +0530

    added new page and made some changes

--- index.js
+++ index.js
@@ -8,11 +8,29 @@ function dropDownAnswer(element) {
         element.parentElement.classList.remove('open');
     }
 }
-function resourcesDropDown(id) {
-    if (id == 'web') {
-        $("#resourcesDropDown").toggleClass("hidden")
-    } else {
-        $("#resourcesDropDownMobile").toggleClass("hidden")
-
+function resourcesDropDown(id, name) {
+    if (id == 'web' && name == 'resouce') {
+        $("#resourcesDropDownResouce").toggleClass("hidden")
     }
+    else if  (id == 'web' && name == 'college') {
+        $("#resourcesDropDownCollege").toggleClass("hidden")
+    }
+    else if  (id == 'phone' && name == 'resouce') {
+        $("#resourcesDropDownMobileResource").toggleClass("hidden")
+    }
+     else {
+        $("#resourcesDropDownMobileCollege").toggleClass("hidden")
+    }
+}
+
+function lessDisplay(){
+    $(".mentorBoxes").addClass("hidden")
+    $(".less").addClass("hidden")
+    $(".more").removeClass("hidden")
+
+}
+function displayMore(){
+    $(".mentorBoxes").removeClass("hidden")
+    $(".more").addClass("hidden")
+    $(".less").removeClass("hidden")
 }
