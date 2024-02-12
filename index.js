8439126eaf6c1e566fc8296a87b4f2f149e507c2
mymaarg <mymaarg@gmail.com>
2024-02-12 14:31:53 +0530

    Mentor Sequence changed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mentorObj;
-var mentorsOrder =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+// var mentorsOrder =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+var mentorsOrder =[4,5,11,12,13,15,0,2,3,8,9,10,14,16,17,18,19]
 var filterApplied = false
 let clonedMentorObj
 let experiencedFiltered="";
